41371d782666812e861102f6eaaffbae5ba669b7
STARTRACEX <startraceebr@foxmail.com>
2025-06-18 23:20:34 +0800

    stay similar to create vite

--- starter/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from 'riot'
-import registerGlobalComponents from './register-global-components'
-import './style.css'
-
-// register
-registerGlobalComponents()
-
-// mount all the global components found in this page
-mount('[data-riot-component]')
--- starter/vite.config.ts
+++ starter/vite.config.ts
@@ -1,13 +1,5 @@
 import riot from '@your-riot/rollup-plugin'
 
 export default {
-  test: {
-    environment: 'happy-dom',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-    },
-    include: ['**/*.test.ts', '**/*.spec.ts'],
-  },
   plugins: [riot()],
 }

7a848127d8f94bc73042ef5d9fc8786b9bf69fd8
STARTRACEX <startraceebr@foxmail.com>
2025-06-19 15:51:13 +0800

    remove useless as const

--- packages/parser/src/constants.ts
+++ packages/parser/src/constants.ts
@@ -1,16 +1,16 @@
-export const JAVASCRIPT_OUTPUT_NAME = 'javascript' as const
-export const CSS_OUTPUT_NAME = 'css' as const
-export const TEMPLATE_OUTPUT_NAME = 'template' as const
+export const JAVASCRIPT_OUTPUT_NAME = 'javascript'
+export const CSS_OUTPUT_NAME = 'css'
+export const TEMPLATE_OUTPUT_NAME = 'template'
 
 // Tag names
-export const JAVASCRIPT_TAG = 'script' as const
-export const STYLE_TAG = 'style' as const
-export const TEXTAREA_TAG = 'textarea' as const
+export const JAVASCRIPT_TAG = 'script'
+export const STYLE_TAG = 'style'
+export const TEXTAREA_TAG = 'textarea'
 
 // Boolean attributes
-export const IS_RAW = 'isRaw' as const
-export const IS_SELF_CLOSING = 'isSelfClosing' as const
-export const IS_VOID = 'isVoid' as const
-export const IS_BOOLEAN = 'isBoolean' as const
-export const IS_CUSTOM = 'isCustom' as const
-export const IS_SPREAD = 'isSpread' as const
+export const IS_RAW = 'isRaw'
+export const IS_SELF_CLOSING = 'isSelfClosing'
+export const IS_VOID = 'isVoid'
+export const IS_BOOLEAN = 'isBoolean'
+export const IS_CUSTOM = 'isCustom'
+export const IS_SPREAD = 'isSpread'
--- packages/parser/src/node-types.ts
+++ packages/parser/src/node-types.ts
@@ -1,8 +1,8 @@
-export const TAG = 1 as const /* TAG */
-export const ATTR = 2 as const /* ATTR */
-export const TEXT = 3 as const /* TEXT */
-export const CDATA = 4 as const /* CDATA */
-export const COMMENT = 8 as const /* COMMENT */
-export const DOCUMENT = 9 as const /* DOCUMENT */
-export const DOCTYPE = 10 as const /* DOCTYPE */
-export const DOCUMENT_FRAGMENT = 11 as const /* DOCUMENT_FRAGMENT */
+export const TAG = 1 /* TAG */
+export const ATTR = 2 /* ATTR */
+export const TEXT = 3 /* TEXT */
+export const CDATA = 4 /* CDATA */
+export const COMMENT = 8 /* COMMENT */
+export const DOCUMENT = 9 /* DOCUMENT */
+export const DOCTYPE = 10 /* DOCTYPE */
+export const DOCUMENT_FRAGMENT = 11 /* DOCUMENT_FRAGMENT */

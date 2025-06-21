e6baab535de50478d3ac775d3100a6532621a2be
STARTRACEX <startraceebr@foxmail.com>
2025-06-22 03:18:37 +0800

    remove type tests

--- packages/compiler/test/generators/javascript.spec.js
+++ packages/compiler/test/generators/javascript.spec.js
@@ -80,152 +80,152 @@ ctx.name = 'hello'
 </script>
 `;
 
-const interfacesExport = `
-<script lang='ts'>
-import {withTypes, RiotComponent} from 'riot'
+// const interfacesExport = `
+// <script lang='ts'>
+// import {withTypes, RiotComponent} from 'riot'
 
-export interface ARandomInterface {
-    foo: string
-    bar: number
-}
-  
-export interface MyComponentProps {
-  username: string
-}
-  
-export type MyComponentState = {
-  message: string
-}
-  
-export interface ComponentInterface extends RiotComponent<MyComponentProps, MyComponentState> {
-    onClick(event: MouseEvent): void
-}
-  
-export default withTypes<ComponentInterface>({
-    state: {
-        message: 'hello'
-    },
-    onClick(event: MouseEvent) {
-        this.update({
-            message: 'goodbye'
-        })
-    }
-})
-</script>
-`;
+// export interface ARandomInterface {
+//     foo: string
+//     bar: number
+// }
 
-const interfacesExportWithoutRiotImport = `
-<script lang='ts'>
-export interface ARandomInterface {
-    foo: string
-    bar: number
-}
-  
-export interface MyComponentProps {
-  username: string
-}
-  
-export type MyComponentState = {
-  message: string
-}
-  
-export interface ComponentInterface extends RiotComponent<MyComponentProps, MyComponentState> {
-    onClick(event: MouseEvent): void
-}
-  
-export default withTypes<ComponentInterface>({
-    state: {
-        message: 'hello'
-    },
-    onClick(event: MouseEvent) {
-        this.update({
-            message: 'goodbye'
-        })
-    }
-})
-</script>
-`;
+// export interface MyComponentProps {
+//   username: string
+// }
 
-const typesExport = `
-<script lang="ts">
-  import {withTypes, RiotComponent} from 'riot'
+// export type MyComponentState = {
+//   message: string
+// }
 
-  export type MyComponentProps = {
-    username: string
-  }
+// export interface ComponentInterface extends RiotComponent<MyComponentProps, MyComponentState> {
+//     onClick(event: MouseEvent): void
+// }
 
-  export interface MyComponentState {
-    message: string
-  }
+// export default withTypes<ComponentInterface>({
+//     state: {
+//         message: 'hello'
+//     },
+//     onClick(event: MouseEvent) {
+//         this.update({
+//             message: 'goodbye'
+//         })
+//     }
+// })
+// </script>
+// `;
 
-  export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState> & {
-    onClick(event: MouseEvent): void
-  }
-        
-  export default withTypes<ComponentInterface>({
-    state: {
-      message: 'hello'
-    },
-    onClick(event: MouseEvent) {
-      this.update({
-        message: 'goodbye'
-      })
-    }
-  })
-</script>
-`;
+// const interfacesExportWithoutRiotImport = `
+// <script lang='ts'>
+// export interface ARandomInterface {
+//     foo: string
+//     bar: number
+// }
 
-const typesExportWithoutRiotImport = `
-<script lang="ts">
-  export type MyComponentProps = {
-    username: string
-  }
+// export interface MyComponentProps {
+//   username: string
+// }
 
-  export interface MyComponentState {
-    message: string
-  }
+// export type MyComponentState = {
+//   message: string
+// }
 
-  export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState> & {
-    onClick(event: MouseEvent): void
-  }
-        
-  export default withTypes<ComponentInterface>({
-    state: {
-      message: 'hello'
-    },
-    onClick(event: MouseEvent) {
-      this.update({
-        message: 'goodbye'
-      })
-    }
-  })
-</script>
-`;
+// export interface ComponentInterface extends RiotComponent<MyComponentProps, MyComponentState> {
+//     onClick(event: MouseEvent): void
+// }
 
-const typesAliasExportWithoutRiotImport = `
-<script lang="ts">
-  export type MyComponentProps = {
-    username: string
-  }
+// export default withTypes<ComponentInterface>({
+//     state: {
+//         message: 'hello'
+//     },
+//     onClick(event: MouseEvent) {
+//         this.update({
+//             message: 'goodbye'
+//         })
+//     }
+// })
+// </script>
+// `;
 
-  export interface MyComponentState {
-    message: string
-  }
+// const typesExport = `
+// <script lang="ts">
+//   import {withTypes, RiotComponent} from 'riot'
 
-  export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState>
-        
-  export default withTypes<ComponentInterface>({
-    state: {
-      message: 'hello'
-    },
-    onClick(event: MouseEvent) {
-      this.update({
-        message: 'goodbye'
-      })
-    }
-  })
-</script>
-`;
+//   export type MyComponentProps = {
+//     username: string
+//   }
+
+//   export interface MyComponentState {
+//     message: string
+//   }
+
+//   export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState> & {
+//     onClick(event: MouseEvent): void
+//   }
+
+//   export default withTypes<ComponentInterface>({
+//     state: {
+//       message: 'hello'
+//     },
+//     onClick(event: MouseEvent) {
+//       this.update({
+//         message: 'goodbye'
+//       })
+//     }
+//   })
+// </script>
+// `;
+
+// const typesExportWithoutRiotImport = `
+// <script lang="ts">
+//   export type MyComponentProps = {
+//     username: string
+//   }
+
+//   export interface MyComponentState {
+//     message: string
+//   }
+
+//   export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState> & {
+//     onClick(event: MouseEvent): void
+//   }
+
+//   export default withTypes<ComponentInterface>({
+//     state: {
+//       message: 'hello'
+//     },
+//     onClick(event: MouseEvent) {
+//       this.update({
+//         message: 'goodbye'
+//       })
+//     }
+//   })
+// </script>
+// `;
+
+// const typesAliasExportWithoutRiotImport = `
+// <script lang="ts">
+//   export type MyComponentProps = {
+//     username: string
+//   }
+
+//   export interface MyComponentState {
+//     message: string
+//   }
+
+//   export type ComponentInterface = RiotComponent<MyComponentProps, MyComponentState>
+
+//   export default withTypes<ComponentInterface>({
+//     state: {
+//       message: 'hello'
+//     },
+//     onClick(event: MouseEvent) {
+//       this.update({
+//         message: 'goodbye'
+//       })
+//     }
+//   })
+// </script>
+// `;
 
 const FAKE_FILE = "fake-file.js";
 
@@ -343,111 +343,111 @@ describe("Generators - javascript", () => {
     expect(output.default.template).to.be.not.ok;
   });
 
-  it("Component interface export can be detected and transformed", () => {
-    const { javascript } = parser().parse(interfacesExport).output;
-    const ast = compileJavascript(
-      javascript,
-      interfacesExport,
-      {
-        options: {
-          file: FAKE_FILE,
-        },
-      },
-      createInput(),
-    );
-    const { code } = print(ast);
+  // it("Component interface export can be detected and transformed", () => {
+  //   const { javascript } = parser().parse(interfacesExport).output;
+  //   const ast = compileJavascript(
+  //     javascript,
+  //     interfacesExport,
+  //     {
+  //       options: {
+  //         file: FAKE_FILE,
+  //       },
+  //     },
+  //     createInput(),
+  //   );
+  //   const { code } = print(ast);
 
-    expect(code).to.be.a("string");
+  //   expect(code).to.be.a("string");
 
-    expect(code).to.contain(
-      "import { withTypes, RiotComponent, RiotComponentWrapper } from 'riot'",
-    );
-    expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
-  });
+  //   expect(code).to.contain(
+  //     "import { withTypes, RiotComponent, RiotComponentWrapper } from 'riot'",
+  //   );
+  //   expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
+  // });
 
-  it("Component type export can be detected and transformed", () => {
-    const { javascript } = parser().parse(typesExport).output;
-    const ast = compileJavascript(
-      javascript,
-      typesExport,
-      {
-        options: {
-          file: FAKE_FILE,
-        },
-      },
-      createInput(),
-    );
-    const { code } = print(ast);
+  // it("Component type export can be detected and transformed", () => {
+  //   const { javascript } = parser().parse(typesExport).output;
+  //   const ast = compileJavascript(
+  //     javascript,
+  //     typesExport,
+  //     {
+  //       options: {
+  //         file: FAKE_FILE,
+  //       },
+  //     },
+  //     createInput(),
+  //   );
+  //   const { code } = print(ast);
 
-    expect(code).to.be.a("string");
+  //   expect(code).to.be.a("string");
 
-    expect(code).to.contain(
-      "import { withTypes, RiotComponent, RiotComponentWrapper } from 'riot'",
-    );
-    expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
-  });
+  //   expect(code).to.contain(
+  //     "import { withTypes, RiotComponent, RiotComponentWrapper } from 'riot'",
+  //   );
+  //   expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
+  // });
 
-  it("Component interface export can be detected and transformed", () => {
-    const { javascript } = parser().parse(
-      interfacesExportWithoutRiotImport,
-    ).output;
-    const ast = compileJavascript(
-      javascript,
-      interfacesExportWithoutRiotImport,
-      {
-        options: {
-          file: FAKE_FILE,
-        },
-      },
-      createInput(),
-    );
-    const { code } = print(ast);
+  // it("Component interface export can be detected and transformed", () => {
+  //   const { javascript } = parser().parse(
+  //     interfacesExportWithoutRiotImport,
+  //   ).output;
+  //   const ast = compileJavascript(
+  //     javascript,
+  //     interfacesExportWithoutRiotImport,
+  //     {
+  //       options: {
+  //         file: FAKE_FILE,
+  //       },
+  //     },
+  //     createInput(),
+  //   );
+  //   const { code } = print(ast);
 
-    expect(code).to.be.a("string");
+  //   expect(code).to.be.a("string");
 
-    expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
-    expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
-  });
+  //   expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
+  //   expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
+  // });
 
-  it("Component type export can be detected and transformed", () => {
-    const { javascript } = parser().parse(typesExportWithoutRiotImport).output;
-    const ast = compileJavascript(
-      javascript,
-      typesExportWithoutRiotImport,
-      {
-        options: {
-          file: FAKE_FILE,
-        },
-      },
-      createInput(),
-    );
-    const { code } = print(ast);
+  // it("Component type export can be detected and transformed", () => {
+  //   const { javascript } = parser().parse(typesExportWithoutRiotImport).output;
+  //   const ast = compileJavascript(
+  //     javascript,
+  //     typesExportWithoutRiotImport,
+  //     {
+  //       options: {
+  //         file: FAKE_FILE,
+  //       },
+  //     },
+  //     createInput(),
+  //   );
+  //   const { code } = print(ast);
 
-    expect(code).to.be.a("string");
+  //   expect(code).to.be.a("string");
 
-    expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
-    expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
-  });
+  //   expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
+  //   expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
+  // });
 
-  it("Component alias type export can be detected and transformed", () => {
-    const { javascript } = parser().parse(
-      typesAliasExportWithoutRiotImport,
-    ).output;
-    const ast = compileJavascript(
-      javascript,
-      typesAliasExportWithoutRiotImport,
-      {
-        options: {
-          file: FAKE_FILE,
-        },
-      },
-      createInput(),
-    );
-    const { code } = print(ast);
+  // it("Component alias type export can be detected and transformed", () => {
+  //   const { javascript } = parser().parse(
+  //     typesAliasExportWithoutRiotImport,
+  //   ).output;
+  //   const ast = compileJavascript(
+  //     javascript,
+  //     typesAliasExportWithoutRiotImport,
+  //     {
+  //       options: {
+  //         file: FAKE_FILE,
+  //       },
+  //     },
+  //     createInput(),
+  //   );
+  //   const { code } = print(ast);
 
-    expect(code).to.be.a("string");
+  //   expect(code).to.be.a("string");
 
-    expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
-    expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
-  });
+  //   expect(code).to.contain('import { RiotComponentWrapper } from "riot"');
+  //   expect(code).to.contain("} as RiotComponentWrapper<ComponentInterface>;");
+  // });
 });
